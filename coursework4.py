f39c7a5b09e937fb24793126f06b52b207f89ee7
BallMJ92 <33960596+BallMJ92@users.noreply.github.com>
2018-03-13 15:53:03 +0000

    Update coursework4.py

--- coursework4.py
+++ coursework4.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
 
-
 root = tk.Tk()
 wtitle = root.title("Threshold")
 winWidth = 1200
@@ -13,25 +12,21 @@ h = 600
 class App:
     
     "Defining Menu"
-    def menuFunction():       
-        
+    def menuFunction():     
        "Initializing menu" 
        menubar=tk.Menu(root)
-
        "Initializing drop-down menu elements" 
        filemenu=tk.Menu(menubar,tearoff=0)
        filemenu.add_command(label="Open Image", command=openFile())
        filemenu.add_command(label="Save Image")
        filemenu.add_command(label="Exit",command=root.destroy)
-
        "Initializing File button" 
        menubar.add_cascade(label="File",menu=filemenu)
        root.config(menu=menubar)
 
     def frameLeft():       
         left_frame = tk.Frame(root, width = w, height = h, bg = 'yellow')    
-        left_frame.pack(side = tk.LEFT, expand = True, fill = tk.BOTH)
-    
+        left_frame.pack(side = tk.LEFT, expand = True, fill = tk.BOTH)    
 
     def frameRight():
         right_frame = tk.Frame(root, width = w, height = h, bg = 'red')
@@ -41,11 +36,8 @@ class App:
         filename = askopenfilename()
         return filename       
 
-        
-
 App.menuFunction()
 App.frameLeft()
 App.frameRight()
-
 root.pack_propagate(0)
 root.mainloop()
