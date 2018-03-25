0366d2dda86c44d205354e3b4a1e059b59f3a880
BallMJ92 <33960596+BallMJ92@users.noreply.github.com>
2018-03-25 21:05:28 +0100

    Update GreyScaleImage.py

--- GreyScaleImage.py
+++ GreyScaleImage.py
@@ -3,10 +3,13 @@ from BinaryImage import BinaryImage
 
 class GreyScaleImage(GUIconnect):
 
+   t = 0  
    def _openGreyScaleImage(self,filename):
+      filetype = "Greyscale Image"
       inVals = []
       outVals = []
-
+      grey_v = 0
+      
       try:
          with open(filename) as input_file:
             fline = input_file.readline()
@@ -22,14 +25,21 @@ class GreyScaleImage(GUIconnect):
          x = x.replace(",", "")
          y = y.replace(",", "")
          v = v.replace(",", "")
+         grey_v += int(v)
          vals = int(x), int(y), str(self._determineColorValue(int(v)))
          outVals.append(vals)
-         
-      return outVals   
+
+      
+      self.value = grey_v//len(inVals)
+
+      return outVals, GUIconnect.getThreshold(self, self.value)   
          
    def _determineColorValue(self,v):
-            return ("#%02x%02x%02x" % (v, v, v))
-
+      return ("#%02x%02x%02x" % (v, v, v))
+   
+   def threshold(self):
+      return t
+   
    def main(self):
          self._openGreyScaleImage("GreyImage.txt")
 
