6d0c8bf219ff5c322c2d4f7762dcea536067134c
BallMJ92 <33960596+BallMJ92@users.noreply.github.com>
2018-03-28 15:59:29 +0100

    Update GreyScaleImage.py

--- GreyScaleImage.py
+++ GreyScaleImage.py
@@ -11,44 +11,75 @@ class GreyScaleImage(GUIconnect):
       for i in t:
          v+=int(i)
 
-      x = int(v//len(t))
+      self.x = int(v//len(t))
 
-      """for i in t:
-         if i<x:
-            binaryVals.append("""
-      return x
+      
+
+      """for index in range(2, len(c), 3):
+         c[index] = int(x)
+
+      print(c[0:50])
+
+      c = [c[i:i+3] for i in range(0, len(c), 3)]
+      
+      print(c[0:50])"""
+
+      return self.x
+      
+   def binariseImage(self):
+
+      """for index in range(2, len(self.inVals), 3):
+          n = int(self.inVals[index])
+          if n >= self.x:
+             self.inVals[index] = int(1)
+          else:
+             self.inVals[index] = int(0)
+
+      print(self.inVals[0:50])"""
+      
+      self.t = [1 if i >= self.x else 0 for i in self.t]
+      print(self.t[0:50])
+      b = []
+
    
-   def _openGreyScaleImage(self,filename):
-      inVals = []
-      outVals = []
-      t = []
+   def dataForDisplay(self, filename):
+      self.inVals = []
+      self.outVals = []
+      self.t = []
+      self.coordinates = []
+      xyCoordinates = []
       
       try:
          with open(filename) as input_file:
             fline = input_file.readline()
             if fline.strip() == "Greyscale Image":
                for line in input_file:
-                  inVals.append(line.split())
+                  self.inVals.append(line.split())
          input_file.close()
       except Exception:
          pass
 
-      for i in inVals:
+      for i in self.inVals:
          x, y, v = i
          x = x.replace(",", "")
          y = y.replace(",", "")
          v = v.replace(",", "")
          vals = int(x), int(y), str(self._determineColorValue(int(v)))
-         t.append(int(v))
-         outVals.append(vals)
+         self.t.append(int(v))
+         self.coordinates.extend((int(x), int(y)))
+         self.outVals.append(vals)
 
-      return outVals, self.getThreshold(t), t   
+      print("In vals")
+      print(self.inVals[0:50])
+      print("------")
+      return self.outVals, self.getThreshold(self.t), self.t   
          
    def _determineColorValue(self,v):
             return ("#%02x%02x%02x" % (v, v, v))
 
    def main(self):
-         self._openGreyScaleImage("GreyImage.txt")
+         self.dataForDisplay("GreyImage.txt")[1]
+         self.binariseImage()
 
 if __name__ == "__main__":
    c = GreyScaleImage()
