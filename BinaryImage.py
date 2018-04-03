3688e7e0b11efd2f083ded8ef9efcb4e82b0c0b1
BallMJ92 <33960596+BallMJ92@users.noreply.github.com>
2018-04-03 22:53:48 +0100

    Create BinaryImage.py

---
+++ BinaryImage.py
@@ -0,0 +1,15 @@
+class BinaryImage:
+
+       def determinePixelValue(self, vals):
+           image = []
+           for i in vals:
+               x, y, v = i
+               vals = int(x), int(y), self._determineColorValue(int(v))
+               image.append(vals)
+
+           return image
+            
+       def _determineColorValue(self,b):
+            v = 255*b
+            return ("#%02x%02x%02x" % (v, v, v))
+
