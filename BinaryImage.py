c1aeede029a75e3cdea548a383110a98f37b86fb
BallMJ92 <33960596+BallMJ92@users.noreply.github.com>
2018-04-04 14:26:03 +0100

    Update BinaryImage.py

--- BinaryImage.py
+++ BinaryImage.py
@@ -1,10 +1,16 @@
+# Matthew Ball MSc Information Technology 04/04/2018
 class BinaryImage:
 
        def determinePixelValue(self, vals):
            image = []
+
+           # Iterating over vals list
            for i in vals:
+               # Extracting data from individual list and assigning to variables
                x, y, v = i
+               # Reassigning variables and determined colour value to vals variable
                vals = int(x), int(y), self._determineColorValue(int(v))
+               # Appending data to new image list
                image.append(vals)
 
            return image
@@ -12,4 +18,3 @@ class BinaryImage:
        def _determineColorValue(self,b):
             v = 255*b
             return ("#%02x%02x%02x" % (v, v, v))
-
